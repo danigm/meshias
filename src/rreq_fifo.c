e73ac752e84760560e4b3c4d083ebbe78e14fa79
Eduardo <edulix@edulix-netbook>
2009-03-01 11:20:21 +0100

    working in the rrep fifo queue

--- src/rreq_fifo.c
+++ src/rreq_fifo.c
@@ -1,29 +1,70 @@
 #include "rreq_fifo.h"
 #include <stdlib.h>
 
+void __rreq_fifo_alarm_cb(struct alarm_block* alarm, void *data)
+{
+    
+}
+
 struct rreq_fifo* rreq_fifo_alloc()
 {
     struct rreq_fifo* queue = (struct rreq_fifo*)
         malloc(sizeof(struct rreq_fifo*));
+        
+    init_alarm(&queue->alarm, queue, __rreq_fifo_alarm_cb);
+    INIT_LIST_HEAD(&(queue->list));
+    
     return queue;
 }
 
 void rreq_fifo_delete(struct rreq_fifo* queue)
 {
+    struct rreq_fifo *entry, *tmp;
+    
+    list_for_each_entry_safe(entry, tmp, &queue->list, list)
+    {
+        list_del(&entry->list);
+        free(entry);
+    }
     free(queue);
 }
 
-void rreq_fifo_push(struct rreq_fifo* queue, uint32_t id)
+void rreq_fifo_push(struct rreq_fifo* queue, uint32_t rreq_id,
+    struct in_addr dst)
 {
+    struct rreq_fifo* entry = (struct rreq_fifo*)
+        malloc(sizeof(struct rreq_fifo*));
     
+    entry->rreq_id = rreq_id;
+    entry->dst = dst;
+    
+    list_add(&entry->list, &queue->list);
 }
 
-uint32_t rreq_fifo_pull(struct rreq_fifo* queue)
+uint8_t rreq_fifo_contains(struct rreq_fifo* queue, uint32_t rreq_id,
+    struct in_addr dst)
 {
+    struct rreq_fifo *entry;
+    
+    list_for_each_entry(entry, &queue->list, list)
+    {
+        if(entry->rreq_id == rreq_id && entry->dst.s_addr == dst.s_addr)
+            return 1;
+    }
     return 0;
 }
 
-uint8_t rreq_fifo_is_empty(struct rreq_fifo* queue)
+void rreq_fifo_del(struct rreq_fifo* queue, uint32_t rreq_id,
+    struct in_addr dst)
 {
-    return 0;
+    struct rreq_fifo *entry, *tmp;
+    
+    list_for_each_entry_safe(entry, tmp, &queue->list, list)
+    {
+        if(entry->rreq_id == rreq_id && entry->dst.s_addr == dst.s_addr)
+        {
+            list_del(&entry->list);
+            return;
+        }
+    }
 }
--- src/rreq_fifo.h
+++ src/rreq_fifo.h
@@ -2,10 +2,17 @@
 #define RREQ_FIFO_H_
 
 #include <stdint.h>
+#include <netinet/in.h>
+#include "alarm/linux_list.h"
+#include "alarm/alarm.h"
 
 struct rreq_fifo
 {
-    uint32_t id;
+    uint32_t rreq_id;
+    struct in_addr dst;
+    
+    struct list_head list;
+    struct alarm_block alarm;
 };
 
 
@@ -22,16 +29,20 @@ void rreq_fifo_delete(struct rreq_fifo* queue);
 /**
  * Add a new RREQ id to the queue.
  */
-void rreq_fifo_push(struct rreq_fifo* queue, uint32_t id);
+void rreq_fifo_push(struct rreq_fifo* queue, uint32_t rreq_id,
+    struct in_addr dst);
 
 /**
- * Get a RREQ if there's any available, otherwise returns 0.
+ * Returns 1 if the RREQ is buffered, otherwise returns 0.
  */
-uint32_t rreq_fifo_pull(struct rreq_fifo* queue);
+uint8_t rreq_fifo_contains(struct rreq_fifo* queue, uint32_t rreq_id,
+    struct in_addr dst);
 
 /**
- * Returns a non-zero number if the queue is empty or 0 otherwise.
+ * Locates the rreq_fifo entry with the given rreq_id and dst if found in the buffer,
+ * removing the entry from the buffer if found.
  */
-uint8_t rreq_fifo_is_empty(struct rreq_fifo* queue);
+void rreq_fifo_del(struct rreq_fifo* queue, uint32_t rreq_id,
+    struct in_addr dst);
 
 #endif
