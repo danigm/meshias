a1c634a67dc9b8202f2955d479b6153bf3dbfad0
Eduardo <edulix@edulix-netbook>
2009-03-10 17:02:03 +0100

    adding an example which exemplifies how to get the ip from a network interface

---
+++ examples/getip.c
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <ifaddrs.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+
+int main(int argc, char **argv)
+{
+    struct ifaddrs *id;
+    struct sockaddr_in *sin;
+    int val = getifaddrs(&id);
+    
+    while (NULL != id->ifa_next)
+    {
+        if (NULL != id->ifa_addr)
+        {
+            if (AF_INET == id->ifa_addr->sa_family)
+            {
+                sin = (struct sockaddr_in *)(id->ifa_addr);
+                printf ("%s:\n\tip %s\n", id->ifa_name,
+                    inet_ntoa (sin->sin_addr));
+            }
+        }
+        id = id->ifa_next;
+    }
+    
+    return 0;
+}
